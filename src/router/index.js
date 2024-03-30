17864e050dc3bbd918d602ea2d20561e8b789759
Sabry-96 <s.hamed@tenjaz.com>
2024-03-30 23:32:19 +0200

    init route

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import adminRoute from '@/views/Admin/admin.route'
-import merchantsRoute from '@/views/Merchants/merchants.route'
-import serviceProviderRoute from '@/views/ServiceProvider/serviceProvider.route'
-import landingRoute from '@/views/Landing/landing.route'
+import { createRouter, createWebHashHistory } from "vue-router";
+import adminRoute from "@/views/Admin/admin.route";
+import merchantsRoute from "@/views/Merchants/merchants.route";
+import serviceProviderRoute from "@/views/ServiceProvider/serviceProvider.route";
+import landingRoute from "@/views/Landing/landing.route";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     ...adminRoute.routes,
     ...merchantsRoute.routes,
     ...serviceProviderRoute.routes,
     ...landingRoute.routes
   ]
-})
+});
 
-export default router
+export default router;
